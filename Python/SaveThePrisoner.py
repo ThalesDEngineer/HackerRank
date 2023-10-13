9e957d8b9bf3220096566d992ec0ec9ec430e40c
Thales Donizeti <41244763+ThalesDEngineer@users.noreply.github.com>
2023-10-12 23:50:46 -0300

    Create SaveThePrisoner.py

---
+++ Python/SaveThePrisoner.py
@@ -0,0 +1,24 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'saveThePrisoner' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER s
+#
+
+def saveThePrisoner(n, m, s):
+    ans = (s+m-1)%n
+    if ans==0:
+        return n
+
+    return ans
+           
